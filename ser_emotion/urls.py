48fcdc8d704fc0645d4b0e836625c867cb71a9ad
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-07-02 09:19:06 +0100

    Change url path to recognize app

--- ser_emotion/urls.py
+++ ser_emotion/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recognize/', include('recognizer.urls', namespace='recognize')),
+    path('', include('recognizer.urls', namespace='recognize')),
 ]
