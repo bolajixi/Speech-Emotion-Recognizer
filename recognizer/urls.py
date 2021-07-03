68e6e6569c1a06039155fe8f8a6931bd28511438
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-07-03 14:01:56 +0100

    Add url for recording

--- recognizer/urls.py
+++ recognizer/urls.py
@@ -5,5 +5,6 @@ app_name = 'recognize'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('recognize/', views.get_emotion, name='get_emotion'),
+    path('recognize/', views.get_emotion_upload, name='get_emotion'),
+    path('recognize/recording/', views.get_emotion_recording, name='get_emotion_rec'),
 ]
