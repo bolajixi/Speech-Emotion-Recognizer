b1cd891bbb710c12d1e52c6d3e233270c93f1804
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-07-03 14:35:23 +0100

    Fix no shape error

--- recognizer/views.py
+++ recognizer/views.py
@@ -5,6 +5,9 @@ from pathlib import Path
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+
 import opensmile
 
 from recognizer.forms import AudioForm
@@ -16,7 +19,18 @@ def index(request):
     return render(request, 'recognizer/index.html', )
 
 
-def get_emotion(request):
+def get_latest_file_path():
+    HOME_path = Path.cwd() / 'media' / 'recordings'
+
+    if os.listdir(HOME_path):
+        # Check if recording is in recordings directory
+        latest_file = os.path.join(
+            HOME_path, str(os.listdir(HOME_path)[-1]))
+
+    return str(latest_file)
+
+
+def get_emotion_upload(request):
     if request.method == "POST":
 
         filename = request.FILES.get('record').name
@@ -30,23 +44,40 @@ def get_emotion(request):
         if form.is_valid():
             form.save()
 
-            HOME_path = Path.cwd() / 'media' / 'recordings'
+            latest_path = get_latest_file_path()  # Get latest file uploaded
 
-            if os.listdir(HOME_path):
-                # Check if recording is in recordings directory
-                latest_file_recording = os.path.join(
-                    HOME_path, str(os.listdir(HOME_path)[-1]))
+            if latest_path:
 
-                foo = process_audio(latest_file_recording)
+                process_audio(latest_path)
 
                 # Delete file after processing
-                os.remove(latest_file_recording)
+                os.remove(latest_path)
 
             return HttpResponse('Success')
     else:
         form = AudioForm()
 
     return render(request, 'recognizer/get_emotion.html', {'form': form})
+
+
+def get_emotion_recording(request):
+    if request.method == "POST":
+        HOME_path = Path.cwd() / 'media' / 'recordings'
+
+        audio_data = request.FILES.get('data')
+        path = default_storage.save(
+            str(HOME_path) + '/recording.wav', ContentFile(audio_data.read()))
+
+        latest_path = get_latest_file_path()  # Get latest file recorded
+
+        if latest_path:
+
+            process_audio(latest_path)
+
+            # Delete file after processing
+            os.remove(latest_path)
+
+    return render(request, 'recognizer/get_emotion.html', )
 
 
 def process_audio(audio_file):
