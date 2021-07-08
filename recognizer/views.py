7fe93a57207e8f9217aba5bd81780887edb4c99d
Bolaji Olawale <bolaji.olawale007@gmail.com>
2021-07-08 17:32:42 +0100

    Reorganize views

--- recognizer/views.py
+++ recognizer/views.py
@@ -2,16 +2,18 @@ import os
 import glob
 from os import path
 from pathlib import Path
-import speech_recognition as sr
+
+import numpy as np
 
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 
-import opensmile
-
 from recognizer.forms import AudioForm
+
+import speech_recognition as sr
+from .process_audio import extract_features
 
 
 # Create your views here.
@@ -20,15 +22,26 @@ def index(request):
     return render(request, 'recognizer/index.html', )
 
 
-def get_latest_file_path():
-    HOME_path = Path.cwd() / 'media' / 'recordings'
+def get_emotion_recording(request):
+    if request.method == "POST":
+        HOME_path = Path.cwd() / 'media' / 'recordings'
 
-    if os.listdir(HOME_path):
-        # Check if recording is in recordings directory
-        latest_file = os.path.join(
-            HOME_path, str(os.listdir(HOME_path)[-1]))
+        audio_data = request.FILES.get('data')
+        path = default_storage.save(
+            str(HOME_path) + '/recording.wav', ContentFile(audio_data.read()))
 
-    return str(latest_file)
+        recent_file_path = get_latest_file_path()  # Get latest file recorded
+
+        if recent_file_path:
+
+            print(predict_audio(recent_file_path).shape)
+            transcription = get_transcription(recent_file_path)
+            print(transcription)
+
+            # Delete file after processing
+            os.remove(recent_file_path)
+
+    return render(request, 'recognizer/get_emotion.html', )
 
 
 def get_emotion_upload(request):
@@ -45,16 +58,16 @@ def get_emotion_upload(request):
         if form.is_valid():
             form.save()
 
-            latest_path = get_latest_file_path()  # Get latest file uploaded
+            recent_file_path = get_latest_file_path()  # Get latest file uploaded
 
-            if latest_path:
+            if recent_file_path:
 
-                process_audio(latest_path)
-                transcription = get_transcription(latest_path)
+                predict_audio(recent_file_path)
+                transcription = get_transcription(recent_file_path)
                 print(transcription)
 
                 # Delete file after processing
-                os.remove(latest_path)
+                os.remove(recent_file_path)
 
             return HttpResponse('Success')
     else:
@@ -63,37 +76,9 @@ def get_emotion_upload(request):
     return render(request, 'recognizer/get_emotion.html', {'form': form})
 
 
-def get_emotion_recording(request):
-    if request.method == "POST":
-        HOME_path = Path.cwd() / 'media' / 'recordings'
-
-        audio_data = request.FILES.get('data')
-        path = default_storage.save(
-            str(HOME_path) + '/recording.wav', ContentFile(audio_data.read()))
-
-        latest_path = get_latest_file_path()  # Get latest file recorded
-
-        if latest_path:
-
-            process_audio(latest_path)
-            transcription = get_transcription(latest_path)
-            print(transcription)
-
-            # Delete file after processing
-            os.remove(latest_path)
-
-    return render(request, 'recognizer/get_emotion.html', )
-
-
-def process_audio(audio_file):
-
-    smile = opensmile.Smile(
-        feature_set=opensmile.FeatureSet.eGeMAPSv02,
-        feature_level=opensmile.FeatureLevel.LowLevelDescriptors,
-    )
-    processed = smile.process_file(audio_file)
-
-    print(processed.shape)
+def predict_audio(audio_file):
+    features = extract_features(audio_file)
+    return features
 
 
 def get_transcription(audio_file):
@@ -112,3 +97,14 @@ def get_transcription(audio_file):
         transcription.append(trans)
 
     return ''.join(transcription)
+
+
+def get_latest_file_path():
+    HOME_path = Path.cwd() / 'media' / 'recordings'
+
+    if os.listdir(HOME_path):
+        # Check if recording is in recordings directory
+        latest_file = os.path.join(
+            HOME_path, str(os.listdir(HOME_path)[-1]))
+
+    return str(latest_file)
